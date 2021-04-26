f2cc6c02d327c4130d3cdb4a334144af06b5ace9
jana sanjakdar <jana.sanjakdar@hotmail.com>
2021-04-26 21:26:41 +0000

    Automatically backed up by Learn

--- main.js
+++ main.js
@@ -4,33 +4,14 @@ const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
 const hearts = document.getElementsByClassName("like-glyph")
-const errorModal = document.getElementById('modal')
-errorModal.classList.add("hidden")     // added class hidden to error msg
 
-document.addEventListener("DOMContentLoaded", () => {
-  for (const heart of hearts){
-    heart.addEventListener("click", (e) => {
-      mimicServerCall()
-        .then(() => {
-          if (heart.innerHTML == EMPTY_HEART){
-            heart.innerHTML = FULL_HEART
-            heart.className = "activated-heart"
-          } else {
-            heart.innerHTML = EMPTY_HEART
-            heart.className = "like-glyph"
-          }
-        })
-        .catch(error => {
-          errorModal.classList.remove("hidden");
-          const modalMessage = document.querySelector("#modal-message")
-          modalMessage.innerText = error
-          setTimeout(() => {
-            errorModal.classList.add("hidden")
-          }, 5000)
-        })
-    })
-  }
+document.addEventListener('click',(e)=>{
+  mimicServerCall()
+  .catch (error => {
+    
+  })
 });
+    
 
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
