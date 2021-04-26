b43d9c2124e5bdf3d786a349e4879a4dba546c4b
jana sanjakdar <jana.sanjakdar@hotmail.com>
2021-04-26 21:23:17 +0000

    Automatically backed up by Learn

--- main.js
+++ main.js
@@ -7,6 +7,30 @@ const hearts = document.getElementsByClassName("like-glyph")
 const errorModal = document.getElementById('modal')
 errorModal.classList.add("hidden")     // added class hidden to error msg
 
+document.addEventListener("DOMContentLoaded", () => {
+  for (const heart of hearts){
+    heart.addEventListener("click", (e) => {
+      mimicServerCall()
+        .then(() => {
+          if (heart.innerHTML == EMPTY_HEART){
+            heart.innerHTML = FULL_HEART
+            heart.className = "activated-heart"
+          } else {
+            heart.innerHTML = EMPTY_HEART
+            heart.className = "like-glyph"
+          }
+        })
+        .catch(error => {
+          errorModal.classList.remove("hidden");
+          const modalMessage = document.querySelector("#modal-message")
+          modalMessage.innerText = error
+          setTimeout(() => {
+            errorModal.classList.add("hidden")
+          }, 5000)
+        })
+    })
+  }
+});
 
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
